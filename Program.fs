8aa32f8df06cf839a001a71683a5baf4a783de40
Thehx <thehdotx@gmail.com>
2021-04-13 18:53:30 +0300

    Testing poly multiplication now

--- Program.fs
+++ Program.fs
@@ -110,7 +110,10 @@ type IntPolyRing(coeffs : IntegerValueType[]) =
 let main argv =
     let intRing = MathTypes.IntegerUnivariatePolyRing();
 
-    let p1 = MathTypes.IntegerUnivariatePolyRing().Add [| 10I; 3I |] [| 2I; 1I; 5I |]
+    let p1 = MathTypes.IntegerUnivariatePolyRing().Multiply [| 10I; 3I |] [| 2I; 1I; 5I |]
     
-    printfn "f(x) = %s" (intRing.GetString(p1))
+    printfn "%s" (intRing.GetString([| 10I; 3I |]))
+    printfn "  times  "
+    printfn "%s" (intRing.GetString([| 2I; 1I; 5I |]))
+    printfn "  equals %s%s" Environment.NewLine (intRing.GetString(p1))
     0 // return an integer exit code
